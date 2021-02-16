86f1cf509faa91294e71139451a79a1a0437fa04
WarEagle451 <56392435+WarEagle451@users.noreply.github.com>
2021-02-16 17:13:38 -0500

    Setup example

---
+++ example/Log.cpp
@@ -0,0 +1,24 @@
+#include "Log.h"
+
+#include <Leaf/Sinks/ConsoleSink.hpp>
+#include <Leaf/Sinks/BasicFileSink.hpp>
+
+namespace Example
+{
+	Ref<Leaf::Logger> Log::s_CoreLogger;
+
+	void Log::Init()
+	{
+		std::vector<Leaf::SinkPtr> sinks;
+		sinks.emplace_back(std::make_shared<Leaf::Sinks::ConsoleSinkMT>());
+		sinks.emplace_back(std::make_shared<Leaf::Sinks::BasicFileSinkMT>("Example.log"));
+
+		sinks[0]->SetPattern("[%T] %N: %m");
+		sinks[1]->SetPattern("[%T] [%L] %N: %m");
+
+		s_CoreLogger = std::make_shared<Leaf::Logger>("Example", sinks.begin(), sinks.end());
+		s_CoreLogger->SetLevel(Leaf::Severity::Trace);
+
+		Leaf::Register(s_CoreLogger);
+	}
+}
